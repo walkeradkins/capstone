796894405009619ac355eb208d7d88065dd5b123
Walker Adkins <walkeradkins@gmail.com>
2022-07-12 16:45:21 -0500

    error handle

--- react-app/src/Components/AddList/add-list.jsx
+++ react-app/src/Components/AddList/add-list.jsx
@@ -2,17 +2,28 @@ import "./add-list.css";
 import { useRef, useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { createList } from "../../store/lists";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const AddList = ({ props }) => {
   const { showAdd, setShowAdd, workspaceId } = props;
   const dispatch = useDispatch();
   const focusRef = useRef(null);
   const [title, setTitle] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [titleLength, setTitleLength] = useState(title.length > 249);
 
   useEffect(() => {
     if (showAdd) focusRef.current.focus();
-    // return (focusRef.current = null);
   }, [showAdd]);
+
+  useEffect(() => {
+    const validationErrors = [];
+    if (title.length > 249)
+      validationErrors.push("List titles must be 250 characters or less");
+    setTitleLength(title.length > 248);
+    console.log("tititle lenght", titleLength);
+    setErrors(validationErrors);
+  }, [title, dispatch]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -35,32 +46,46 @@ const AddList = ({ props }) => {
   };
 
   return (
-    <div className="add-list__form">
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Enter a list title..."
-          className="add-list__input"
-          onChange={(e) => setTitle(e.target.value)}
-          ref={focusRef}
-        />
-        <div className="add-list__buttons">
-          <button
-            className={
-              title.trim().length ? "add-list__submit" : "disabled__btn"
-            }
-            disabled={!title.trim().length}
-          >
-            Add list
-          </button>
-          <button
-            className="add-list__cancel"
-            onClick={() => setShowAdd(false)}
-          >
-            <span className="material-symbols-outlined">close</span>
-          </button>
-        </div>
-      </form>
-    </div>
+    <>
+      <div className="add-list__form">
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Enter a list title..."
+            className="add-list__input"
+            onChange={(e) => setTitle(e.target.value)}
+            ref={focusRef}
+            maxLength={250}
+          />
+          <div className="add-list__buttons">
+            <button
+              className={
+                errors[0] || !title.trim().length
+                  ? "disabled__btn"
+                  : "add-list__submit"
+              }
+              disabled={errors[0] || !title.length}
+            >
+              Add list
+            </button>
+            <button
+              className="add-list__cancel"
+              onClick={() => setShowAdd(false)}
+            >
+              <span className="material-symbols-outlined">close</span>
+            </button>
+            <CSSTransition
+              in={titleLength}
+              timeout={1000}
+              classNames="list-transition"
+            >
+              <div className="error__container-add-list">
+                <p className="error__text-add_list">{errors[0]}</p>
+              </div>
+            </CSSTransition>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
--- react-app/src/Components/ListName/list_name.jsx
+++ react-app/src/Components/ListName/list_name.jsx
@@ -11,6 +11,19 @@ const ListName = ({ list }) => {
   const [errors, setErrors] = useState([]);
   const [edit, setEdit] = useState(false);
 
+  useEffect(() => {
+    const validationErrors = [];
+    if (content.length > 249) {
+      validationErrors.push(
+        "Please keep list titles to 250 characters or less"
+      );
+    }
+    if (content.trim().length < 1) {
+      validationErrors.push("Please provide a title for your list");
+    }
+    setErrors(validationErrors);
+  }, [content, dispatch]);
+
   const trueEdit = (e) => {
     e.stopPropagation();
     e.target.select();
@@ -22,61 +35,78 @@ const ListName = ({ list }) => {
     setContent(title);
   }, [dispatch]);
 
-  useEffect(() => {
-    if (!edit) return;
+  const closeEdit = (e) => {
+    console.log('lengtth',content.length)
+    if (errors.length || !content.length) {
+      console.log("errrors");
+      setEdit(true);
+      return;
+    }
 
-    const closeEdit = (e) => {
-      const errors = [];
-      setErrors(errors);
-
-      const payload = {
-        title: content.trim(),
-      };
-
-      if (!errors.length) {
-        let updatedName;
-        const updateData = async () => {
-          try {
-            updatedName = await dispatch(updateList(payload, id));
-          } catch (error) {
-            alert(error);
-          }
-        };
-
-        updateData();
-        setEdit(false);
-      }
+    const payload = {
+      title: content.trim(),
     };
 
+    if (!errors.length) {
+      let updatedName;
+      const updateData = async () => {
+        try {
+          updatedName = await dispatch(updateList(payload, id));
+        } catch (error) {
+          alert(error);
+        }
+      };
+
+      updateData();
+      setEdit(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!edit) return;
     document.addEventListener("click", closeEdit);
-    document.addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
+    return () => document.removeEventListener("click", closeEdit);
+  }, [edit, content]);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      if (!errors.length) {
         const listNames = Array.from(
           document.getElementsByClassName("input__active-list")
         );
         listNames.forEach((element) => element.blur());
         return closeEdit(e);
+      } else {
+        e.preventDefault();
+        return;
       }
-    });
-    return () => document.removeEventListener("click", closeEdit);
-  }, [edit, content]);
+    }
+  };
 
   if (!list) return null;
   const { title, id } = list;
 
   return (
-    <div className="list__header-items">
-      <p>{errors[0]}</p>
-      <TextareaAutosize
-        id="listname__input"
-        className={edit ? "input__active-list" : "input__inactive-list"}
-        value={content}
-        onClick={trueEdit}
-        maxLength={249}
-        minRows={1}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <ListDelete list={list} />
+    <div className="list__header-container">
+      {errors[0] && (
+        <div className="error__container">
+          <p className="error__text error__text-add-card">{errors}</p>
+        </div>
+      )}
+      <div className="list__header-items">
+        <TextareaAutosize
+          id="listname__input"
+          className={edit ? "input__active-list" : "input__inactive-list"}
+          value={content}
+          spellCheck={false}
+          onClick={trueEdit}
+          maxLength={250}
+          onKeyPress={handleKeyPress}
+          minRows={1}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <ListDelete list={list} />
+      </div>
     </div>
   );
 };
