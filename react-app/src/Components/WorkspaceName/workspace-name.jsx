0de5bd05e5bc8d211f8762f04c047f8023c4926c
Walker Adkins <walkeradkins@gmail.com>
2022-07-12 20:27:18 -0500

    workspace

--- react-app/src/Components/WorkspaceName/workspace-name.jsx
+++ react-app/src/Components/WorkspaceName/workspace-name.jsx
@@ -154,7 +154,7 @@ const WorkspaceName = ({ workspace }) => {
         }
       />
       <CSSTransition
-        in={errorCheck}``
+        in={errorCheck}
         timeout={500}
         classNames="list-transition"
         unmountOnExit
